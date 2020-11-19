b3cf125df3f8bab35ac5b43d1c327d5df5889b18
ChaiyachetU <chaiyachet.u@gmail.com>
2020-11-19 21:55:13 +0700

    Initialize project

---
+++ public/index.js
@@ -0,0 +1,72 @@
+const contents = document.querySelector("#contents");
+const loading = document.querySelector(".progress");
+
+// Get news by newsAPI.org
+const getNews = async () => {
+  const apiKey = "e048aec5172840b997668269ebd30c4f";
+  const url = `http://newsapi.org/v2/top-headlines?country=th&pageSize=100&apiKey=${apiKey}`;
+
+  const newsResponse = await fetch(url);
+  const newsData = await newsResponse.json();
+
+  return newsData.articles;
+};
+
+// Store last news
+let lastestNews = 0;
+
+// Render data to DOM
+async function addDataToDom() {
+  const newsArticles = await getNews();
+
+  if (lastestNews !== newsArticles.length) {
+    const newsElement = document.createElement("div");
+    newsElement.classList.add("card");
+    newsElement.innerHTML = `
+      <div class="card-content">
+        <span class="card-title">${newsArticles[lastestNews].title}</span>
+        <p>${newsArticles[lastestNews].description}</p>
+      </div>
+      <div class="card-action">
+        <span>ที่มา - ${newsArticles[lastestNews].source.name}</span>
+        <a href="${newsArticles[lastestNews].url}" target="_blank"> อ่านต่อ </a>
+      </div>
+    `;
+
+    // Add news
+    contents.appendChild(newsElement);
+
+    // Remove loading
+    loading.classList.remove("show");
+
+    // update latest news
+    lastestNews++;
+  } else {
+    // Unattach event listeners if no more docs
+    window.removeEventListener("scroll", handleScroll);
+    // Remove loading
+    loading.classList.remove("show");
+  }
+}
+
+// Handle scroll
+const handleScroll = () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (clientHeight + scrollTop >= scrollHeight - 5) {
+    // Show the loading animation
+    loading.classList.add("show");
+    // Add more news data
+    setTimeout(addDataToDom, 1000);
+  }
+};
+
+// Wait for DOM content to load
+window.addEventListener("DOMContentLoaded", () => {
+  addDataToDom();
+  addDataToDom();
+  addDataToDom();
+});
+
+// Load more news with scroll eventlistener
+window.addEventListener("scroll", handleScroll);
