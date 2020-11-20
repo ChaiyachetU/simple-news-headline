6b3fb7e35c9bf6380e0a3ef19c26229adaea4103
ChaiyachetU <chaiyachet.u@gmail.com>
2020-11-20 16:48:52 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const contents = document.querySelector("#contents");
+const loading = document.querySelector(".progress");
+
+// Get news by newsAPI.org
+const getNews = async (category = "general") => {
+  const apiKey = "e048aec5172840b997668269ebd30c4f";
+  const url = `https://newsapi.org/v2/top-headlines?country=th&category=${category}&pageSize=100&apiKey=${apiKey}`;
+
+  // Show loading
+  loading.classList.add("show");
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "ok") {
+        const articles = data.articles;
+        articles.forEach((article) => {
+          setTimeout(function () {
+            addDataToDom(article);
+            // Remove loading
+            loading.classList.remove("show");
+          }, 1000);
+        });
+      } else {
+        console.log(data);
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+// Render news data to DOM
+function addDataToDom(article) {
+  const newsElement = document.createElement("div");
+  newsElement.classList.add("card");
+  newsElement.innerHTML = `
+      <div class="card-content">
+        <span class="card-title">${article.title}</span>
+        <p>${article.description}</p>
+      </div>
+      <div class="card-action">
+        <span>ที่มา - ${article.source.name}</span>
+        <a href="${article.url}" target="_blank"> อ่านต่อ </a>
+      </div>
+    `;
+
+  // Add news
+  contents.appendChild(newsElement);
+}
+
+// Wait for DOM content to load
+window.addEventListener("DOMContentLoaded", () => {
+  // Get news and render
+  getNews();
+});
+
+// Get active menu when click nav menu and get news with category
+const lists = document.querySelectorAll("ul > li");
+lists.forEach((li) => {
+  li.addEventListener("click", function (e) {
+    let current = document.querySelector(".active");
+    current.classList.remove("active");
+
+    e.target.parentElement.classList.add("active");
+
+    let category = e.target.innerText.toLowerCase();
+
+    // Clear contents
+    contents.innerHTML = "";
+
+    // Get news and add to contents
+    getNews(category);
+  });
+});
